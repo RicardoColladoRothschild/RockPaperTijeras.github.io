73f30f9fa951b199ac0cd8d4bcfe76da5bb2d448
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-03 18:02:56 -0400

    Added method for pcPlay()

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let jugador;
 let pc;
-
+let counter;
+let playerPoints;
+let pcPoints;
 function playerTouchTijeras(){
     jugador = "tijeras";
+    pc = pcPlay();
     alert(`Player choice: ${jugador}`);
 }
 
@@ -14,5 +17,20 @@ function playeTouchRock(){
 function playeTouchPaper(){
     jugador="paper";
     alert(`Player choice: ${jugador}`);
+}
+
+
+function pcPlay(){
+    let n = Math.random() * 3;
+        if(n===0){
+            return "tijeras";
+        }else if(n===1){
+            return "paper";
+
+        }else if(n===2){
+            return "rock";
+        }
+     return "rock";   
+        
 }
 
