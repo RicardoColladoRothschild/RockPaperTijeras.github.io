d14d3d7b91df78f835251948cb9f9a2334e21fd2
ricardocollado <ricardocolladorothschild@gmail.com>
2023-04-03 08:47:30 -0400

    create methods to determined player choice

---
+++ script.js
@@ -0,0 +1,18 @@
+let jugador;
+let pc;
+
+function playerTouchTijeras(){
+    jugador = "tijeras";
+    alert(`Player choice: ${jugador}`);
+}
+
+function playeTouchRock(){
+    jugador = "rock";
+        alert(`Player choice: ${jugador}`);
+}
+
+function playeTouchPaper(){
+    jugador="paper";
+    alert(`Player choice: ${jugador}`);
+}
+
